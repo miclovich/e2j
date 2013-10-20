551c2b90fdc804c797806d150129f0a8ece4fcdd
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-20 13:46:23 +0300

    refactored view to stringly handle non-UI input

--- reader/views.py
+++ reader/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseServerError
 from django.shortcuts import render_to_response, RequestContext
 from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
 from .forms import UploadFileForm
 from .utils import convert_book_to_json
 
@@ -11,14 +12,15 @@ class ReadJson(View):
         return HttpResponse("This is just an API.")
 
 
+@csrf_exempt
 def upload_file(request):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed('Only POST based API.')
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             book = form.convert_excel()
             json_response = convert_book_to_json(book)
             return HttpResponse(json_response, mimetype='application/json')
-    else:
-        form = UploadFileForm()
-        return render_to_response('reader/upload.html', {'form': form},
-                                  RequestContext(request))
+        else:
+            return HttpResponseServerError("Invalid API call.")
