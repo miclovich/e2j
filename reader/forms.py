6145fc3e75f2b65d7fb7cd0e89cd2d63ca542908
Victor Miclovich Amooti <vicmiclovich@gmail.com>
2013-10-23 18:41:32 +0300

    chart type field added

--- reader/forms.py
+++ reader/forms.py
@@ -3,12 +3,19 @@ import xlrd
 
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    chart_type = forms.CharField()
+    upload_file = forms.FileField()
     # TODO -> clean this form
 
     def convert_excel(self):
         # TODO -> evaluate process for very large files (use processing)
-        excel_file = self.cleaned_data['file']
+        excel_file = self.cleaned_data['upload_file']
         book = xlrd.open_workbook(file_contents=excel_file.read(),
                                   encoding_override='utf-8')
         return book
+
+    def get_charttype(self):
+        chart_type = self.cleaned_data['chart_type']
+        if chart_type:
+            return chart_type
+        return None
